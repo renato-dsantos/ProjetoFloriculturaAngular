878f0d4aaf7868d9cf850cbabd5d943545879d71
RENATO DOS SANTOS <renato.santos25@senacsp.edu.br>
2025-10-13 20:06:24 -0300

    Criando Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{
+        path: 'cadastro',
+        component: CadastroComponent,
+        title: 'Cadastrado de produtos'
+    }
+
+
+];
