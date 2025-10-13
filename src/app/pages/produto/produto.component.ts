811d0e1f2b884ae428a2b101999e34f3e4a08af4
RENATO DOS SANTOS <renato.santos25@senacsp.edu.br>
2025-10-13 19:49:50 -0300

    Subindo projeto

---
+++ src/app/pages/produto/produto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produto',
+  imports: [],
+  templateUrl: './produto.component.html',
+  styleUrl: './produto.component.css'
+})
+export class ProdutoComponent {
+
+}
