534acbd18762cc79bcdc4481088392f34e135485
RENATO DOS SANTOS <renato.santos25@senacsp.edu.br>
2025-10-13 21:28:23 -0300

    Crindo routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { ProdutoComponent } from './pages/produto/produto.component';
 
 export const routes: Routes = [
 
@@ -7,6 +8,12 @@ export const routes: Routes = [
         path: 'cadastro',
         component: CadastroComponent,
         title: 'Cadastrado de produtos'
+    },
+
+    {
+      path: 'produto',
+        component: ProdutoComponent,
+        title: 'Produtos cadastrados'
     }
 
 
