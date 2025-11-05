e1f341be3b2d768245219e6a7e9d6da7208cf2de
DIEGO SARTI ARAUJO VIANA <diego.saviana@senacsp.edu.br>
2025-11-04 21:55:57 -0300

    adicionei a tela de recuperar senha

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { FavoritoComponent } from './pages/favorito/favorito.component';
 import { CadastrousuarioComponent } from './pages/cadastrousuario/cadastrousuario.component';
-
+import { EsqueceusenhaComponent } from './pages/esqueceusenha/esqueceusenha.component';
 export const routes: Routes = [
 
         {
@@ -44,7 +44,11 @@ export const routes: Routes = [
         path: 'cadastrousuario',
         component: CadastrousuarioComponent,
         title: 'Cadastro-usuario',
-    }
+    },
+    { path: 'esqueceusenha',
+       component: EsqueceusenhaComponent,
+      title: 'Esqueceu-senha',
+     }
 
 
 
---
+++ src/app/pages/esqueceusenha/esqueceusenha.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-esqueceusenha',
+  imports: [],
+  templateUrl: './esqueceusenha.component.html',
+  styleUrl: './esqueceusenha.component.css'
+})
+export class EsqueceusenhaComponent {
+
+}
